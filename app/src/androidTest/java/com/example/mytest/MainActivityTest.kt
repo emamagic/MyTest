5d42ec04d2a076f781d5b9670d843f8b69c08014
developer developer <developermagic7@gmail.com>
2021-03-28 02:10:07 -0700

    step_5_activity_added

---
+++ app/src/androidTest/java/com/example/mytest/MainActivityTest.kt
@@ -0,0 +1,29 @@
+package com.example.mytest
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest{
+
+    private lateinit var scenario: ActivityScenario<MainActivity>
+
+    @Before
+    fun setUp(){
+        scenario = ActivityScenario.launch(MainActivity::class.java)
+       // scenario.moveToState(Lifecycle.State.RESUMED)
+    }
+
+    @Test
+    fun testTextView_MainActivity(){
+        onView(withId(R.id.txt_main)).check(matches(isDisplayed()))
+    }
+
+}
