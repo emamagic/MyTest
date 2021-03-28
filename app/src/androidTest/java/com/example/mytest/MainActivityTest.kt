17bb55db5cebef5959fad622114f84fb5c8d94c2
developer developer <developermagic7@gmail.com>
2021-03-28 02:32:51 -0700

    step_5

--- app/src/androidTest/java/com/example/mytest/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/mytest/MainActivityTest.kt
@@ -4,10 +4,11 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -15,6 +16,10 @@ import org.junit.runner.RunWith
 class MainActivityTest{
 
     private lateinit var scenario: ActivityScenario<MainActivity>
+
+    // the Rule calls before each of fun and this below Rule launch the activity
+//    @get:Rule
+//    val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Before
     fun setUp(){
@@ -25,6 +30,7 @@ class MainActivityTest{
     @Test
     fun testTextView_MainActivity(){
         onView(withId(R.id.txt_main)).perform(click())
+       // onView(withId(R.id.my_fragment_root)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
         onView(withId(R.id.my_fragment_root)).check(matches(isDisplayed()))
     }
 
