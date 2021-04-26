2ea2fe958ecad7e5baa2e2cdc274093c903a23e9
developer developer <developermagic7@gmail.com>
2021-04-26 04:22:08 -0700

    step_7

--- app/src/main/java/com/example/mytest/MainActivity.kt
+++ app/src/main/java/com/example/mytest/MainActivity.kt
@@ -7,7 +7,9 @@ import com.example.mytest.databinding.ActivityMainBinding
 import com.example.mytest.ui.MyFragment
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,5 +22,9 @@ class MainActivity : AppCompatActivity() {
                 setReorderingAllowed(true)
             }
         }
+
     }
+
+
+
 }
--- app/src/main/java/com/example/mytest/ui/MyFragment.kt
+++ app/src/main/java/com/example/mytest/ui/MyFragment.kt
@@ -7,11 +7,22 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.mytest.databinding.FragmentMyBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
 
+@ExperimentalCoroutinesApi
+@ExperimentalStdlibApi
 class MyFragment: Fragment() {
 
     private var _binding: FragmentMyBinding? = null
     private val binding get() = _binding
+    private val list: ArrayList<String> by lazy { ArrayList() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,7 +42,40 @@ class MyFragment: Fragment() {
             Toast.makeText(requireContext(), "$name  $family", Toast.LENGTH_SHORT).show()
             binding?.txtResult?.text = "Successfully"
         }
+
+        runBlocking {
+            val list = foo1()
+            for (x in list) println(x)
+
+            // when we called channel is activated
+            val channel = foo2()
+            for (x in channel) println(x)
+
+            // when we called collect is activated
+            val flow = foo3()
+            flow.collect { x -> println(x) }
+
+        }
     }
+
+    private suspend fun foo1(): List<String> = buildList {
+        list.add("a")
+        list.add("b")
+        list.add("c")
+    }
+
+    private fun CoroutineScope.foo2(): ReceiveChannel<String> = produce {
+        send("a")
+        send("b")
+        send("c")
+    }
+
+    private fun foo3(): Flow<String> = flow {
+        emit("a")
+        emit("b")
+        emit("v")
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
